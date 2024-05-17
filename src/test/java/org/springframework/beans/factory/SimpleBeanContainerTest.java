a707931c5c464f92c4d5c3d12a4267944ae5fb85
WJF <wenjianfeng1220@163.com>
2024-05-17 22:23:47 +0800

    add simple-bean-container

---
+++ src/test/java/org/springframework/beans/factory/SimpleBeanContainerTest.java
@@ -0,0 +1,28 @@
+package org.springframework.beans.factory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+/**
+ * @Author: LiYunxiao
+ * @Date: 2024-05-17-22:06
+ * @Description:
+ */
+public class SimpleBeanContainerTest {
+    @Test
+    public void testCreateBean() throws Exception{
+        BeanFactory beanFactory = new BeanFactory();
+        beanFactory.registerBean("helloService",new HelloService());
+        HelloService helloService = (HelloService) beanFactory.getBean("helloService");
+//        assertThat(helloService).isNotNull();
+//        assertThat(helloService.sayHello()).isEqualTo("hello");
+        helloService.sayHello();
+    }
+    class HelloService {
+        public String sayHello() {
+            System.out.println("hello");
+            return "hello";
+        }
+    }
+}
