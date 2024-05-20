5da433a105b3dfe2403b7b96384b38055dc66cff
WJF <wenjianfeng1220@163.com>
2024-05-20 16:18:42 +0800

    使用XML防止加载beanDefinition

--- src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -1,30 +1,32 @@
 package org.springframework.beans.factory.support;
 
-
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 
 /**
  * @author derekyi
  * @date 2020/11/22
- * @describe 实现BeanFactory中的获取Bean的方法，beanMao定义在DefaultSingletonBeanRegistry中
  */
-public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory {
+public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements ConfigurableBeanFactory {
 
-    // 懒加载，在第一次使用的时候才进行创建bean
-    @Override
-    public Object getBean(String name) throws BeansException {
-        Object bean = getSingleton(name);
-        if (bean != null) {
-            return bean;
-        }
+	@Override
+	public Object getBean(String name) throws BeansException {
+		Object bean = getSingleton(name);
+		if (bean != null) {
+			return bean;
+		}
 
-        BeanDefinition beanDefinition = getBeanDefinition(name);
-        return createBean(name, beanDefinition);
-    }
+		BeanDefinition beanDefinition = getBeanDefinition(name);
+		return createBean(name, beanDefinition);
+	}
 
-    protected abstract Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException;
+	@Override
+	public <T> T getBean(String name, Class<T> requiredType) throws BeansException {
+		return ((T) getBean(name));
+	}
 
-    protected abstract BeanDefinition getBeanDefinition(String beanName) throws BeansException;
+	protected abstract Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException;
+
+	protected abstract BeanDefinition getBeanDefinition(String beanName) throws BeansException;
 }
